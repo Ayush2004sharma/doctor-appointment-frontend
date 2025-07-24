9d9b21b3fda34d4b0251fa1115202a76a70fa42b
Ayush2004sharma <ayush.sharma.cseds.2022@miet.iac.in>
2025-07-24 21:57:15 +0530

    update

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.API || "http://localhost:5000/api",
   withCredentials: true, // optional if backend uses cookies
 });
 
